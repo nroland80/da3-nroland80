b8728126863113428041249a88102952b011697b
nroland80 <nroland@zagmail.gonzaga.edu>
2025-03-02 23:12:33 -0800

    test commit

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import numpy as np
 import pandas as pd
 import utils
+
+from utils import calculate_values
+from utils import mergeing_df
+from utils import day_of_week_info
+
+
+youtube_df = pd.read_csv("youtube_analytics_2-11-24_2-9-25.csv")
+days_df = pd.read_csv("days_of_week_2-11-24_2-9-25.csv")
+
+#confirming dates
+youtube_df.set_index("Date")
+days_df.set_index("Date")
+
+# Get start and end dates from the user input
+start_date = input("Enter start date (MM/DD/YY) [Note if the month or date is single digit only enter that single digit]: ")
+end_date = input("Enter end date (MM/DD/YY) [Note if the month or date is single digit only enter that single digit]: ")
+
+date_slice_df = youtube_df.loc[start_date:end_date]
+#currently working
+column_name = input("Please enter in one of the following column name to calculate a value for: Views\nAverage percentage viewed (%)\nUnique viewers\nSubscribers\nWatch time (hours)\nAverage view duration\nShares\nLikes\nDislikes\nComments added\nImpressions\nImpressions click-through rate (%)\n")
+column_date_slice_df = date_slice_df.loc[:, column_name]
+
+calculate_values(column_date_slice_df)
+mergeing_df(youtube_df, days_df)
+day_of_week_info (youtube_df, days_df, column_name)
--- utils.py
+++ utils.py
@@ -1,24 +1,9 @@
 import numpy as np
 import pandas as pd
 #read files
-youtube_df = pd.read_csv("youtube_analytics_2-11-24_2-9-25.csv")
-days_df = pd.read_csv("days_of_week_2-11-24_2-9-25.csv")
-
-#confirming dates
-youtube_df.set_index("Date")
-days_df.set_index("Date")
-
-# Get start and end dates from the user input
-start_date = input("Enter start date (MM/DD/YY) [Note if the month or date is single digit only enter that single digit]: ")
-end_date = input("Enter end date (MM/DD/YY) [Note if the month or date is single digit only enter that single digit]: ")
-
-date_slice_df = youtube_df.loc[start_date:end_date]
-#currently working
-column_name = input("Please enter in one of the following column name to calculate a value for: Views\nAverage percentage viewed (%)\nUnique viewers\nSubscribers\nWatch time (hours)\nAverage view duration\nShares\nLikes\nDislikes\nComments added\nImpressions\nImpressions click-through rate (%)\n")
-column_date_slice_df = date_slice_df.loc[:, column_name]
 
 #sum, mean, standard deviation, median, smallest value, and largest value
-def calculate_sum(column_date_slice_df):
+def calculate_values(column_date_slice_df):
     column_sum = column_date_slice_df.sum()
     column_mean = column_date_slice_df.mean()  
     column_std_dev = column_date_slice_df.std()  
@@ -36,5 +21,14 @@ def calculate_sum(column_date_slice_df):
 })
 
     statistics.to_csv("statistics.csv", header=False)
+#merged dataframe
+def mergeing_df(youtube_df, days_df):
+    complete_df = pd.merge(youtube_df, days_df, on='Date', how='outer')
+    complete_df.to_csv("complete_table.csv")
+#split-apply-combine
+def day_of_week_info (youtube_df, days_df, column_name):
+    complete_df = pd.merge(youtube_df, days_df, on='Date', how='outer')
+    complete_df_grouped = complete_df.groupby("Day of Week")
 
-calculate_sum(column_date_slice_df)
+    daily_means = complete_df.groupby("Day of Week")[column_name].mean()
+    daily_means.to_csv("daily_breakdown.csv", header=False)
